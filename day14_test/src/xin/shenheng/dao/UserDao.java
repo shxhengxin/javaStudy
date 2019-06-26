365ea4726b744f33ef885320177924dbf4ee5b6c
shenhengxin <shenhengxin@qq.com>
2019-06-26 10:18:56 +0800

    day14-test

---
+++ day14_test/src/xin/shenheng/dao/UserDao.java
@@ -0,0 +1,13 @@
+package xin.shenheng.dao;
+
+import xin.shenheng.domain.User;
+
+/**
+ * 登录方法
+ * @param  loginUser 只有用户名和密码
+ */
+public class UserDao {
+    public  User login(User loginUser){
+        return  null;
+    }
+}
