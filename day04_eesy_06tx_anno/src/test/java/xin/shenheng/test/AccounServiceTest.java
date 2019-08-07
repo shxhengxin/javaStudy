a9c55da211750823d8fe899131868e22757f9b1b
shenhengxin <shenhengxin@qq.com>
2019-08-07 15:52:03 +0800

    spring基于注解的声明式事务控制

---
+++ day04_eesy_06tx_anno/src/test/java/xin/shenheng/test/AccounServiceTest.java
@@ -0,0 +1,20 @@
+package xin.shenheng.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import xin.shenheng.service.IAccountService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
+public class AccounServiceTest {
+    @Autowired
+    private IAccountService as;
+
+    @Test
+    public void testTransfer(){
+        as.transfer("aaa","bbb",100f);
+    }
+}
