8c0d6406ada384fe1ee89e502f263ff63f799b9a
shenhengxin <shenhengxin@qq.com>
2019-08-30 01:12:44 +0800

    搭建spring mvc 框架测试

--- ssm/src/main/java/xin/shenheng/controller/AccountController.java
+++ ssm/src/main/java/xin/shenheng/controller/AccountController.java
@@ -1,4 +1,27 @@
 package xin.shenheng.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import xin.shenheng.domain.Account;
+import xin.shenheng.service.AccountService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @RequestMapping("/findAll")
+    public String findAll(){
+
+        System.out.println("findAll执行了....");
+        List<Account> list = accountService.findAll();
+        System.out.println(list);
+
+        return "list";
+    }
 }
--- ssm/src/main/webapp/WEB-INF/pages/list.jsp
+++ ssm/src/main/webapp/WEB-INF/pages/list.jsp
@@ -2,15 +2,16 @@
   Created by IntelliJ IDEA.
   User: Administrator
   Date: 2019/8/30
-  Time: 0:49
+  Time: 1:02
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jstl/core" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-<a href="account/findAll">测试</a>
+<h3>查询所有帐户</h3>
 </body>
 </html>
